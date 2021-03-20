05f79ebd53c01010fa594b41793f387ebfa39eef
joaocmd <joaodavid1999@hotmail.com>
2021-03-20 16:13:56 +0000

    Add vision markers

--- Assets/Scripts/ProximitySensor.cs
+++ Assets/Scripts/ProximitySensor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,44 @@ public class ProximitySensor : MonoBehaviour
 {
 
     public Dictionary<string, Vector3> Seen = new Dictionary<string, Vector3>();
+    public float noise = 0.5f;
+    public int tickRate = 500;
+    public GameObject marker;
+    public Dictionary<string, GameObject> markers = new Dictionary<string, GameObject>();
+
+    private DateTime lastTick;
+
+    void Start() {
+        lastTick = DateTime.Now;
+    }
 
     void OnTriggerExit2D(Collider2D col)
     {
-        Seen.Remove(col.gameObject.name);
+        var name = col.gameObject.name;
+        Seen.Remove(name);
+        GameObject.Destroy(markers[name]);
+        markers.Remove(name);
     }
 
     void OnTriggerStay2D(Collider2D col)
     {
-        Seen[col.gameObject.name] = col.transform.position;
+        Debug.Log((DateTime.Now - lastTick).TotalMilliseconds);
+        if ((DateTime.Now - lastTick).TotalMilliseconds < tickRate) {
+            return;
+        }
+
+        var name = col.gameObject.name;
+        var pos = new Vector3(
+            col.transform.position.x + UnityEngine.Random.Range(-noise, noise),
+            col.transform.position.y + UnityEngine.Random.Range(-noise, noise),
+            col.transform.position.z);
+
+        Seen[name] = pos;
+        if (markers.ContainsKey(name)) {
+            markers[name].transform.position = pos;
+        } else {
+            markers[name] = Instantiate(marker, pos, Quaternion.identity);
+        }
+        lastTick = DateTime.Now;
     }
 }
--- Assets/Scripts/Ship/ShipMovement.cs
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour {
 
-    public static float rotationSpeed = 80f;
+    public static float rotationSpeed = 70f;
     public static float speed = 1.8f;
 
     // TODO: make this private
