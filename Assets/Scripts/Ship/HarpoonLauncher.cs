53f976d88b1b6e4a7af7513a45206834df16720b
Marcelo Mouta <fc51110@alunos.fc.ul.pt>
2021-05-07 17:43:35 +0100

    insert noise when firing the harpoon

--- Assets/Scripts/Ship/HarpoonLauncher.cs
+++ Assets/Scripts/Ship/HarpoonLauncher.cs
@@ -9,32 +9,45 @@ public class HarpoonLauncher : MonoBehaviour
     public SpriteRenderer sprite;
     public GameObject harpoonPrefab;
     public GameObject parent;
+    public float noise = 2f;
 
     public float Timer { get; private set; } = 0f;
 
-    void Update() {
+    void Update()
+    {
         Timer -= Time.deltaTime;
-        if (Timer <= 0f && !sprite.enabled) {
+        if (Timer <= 0f && !sprite.enabled)
+        {
             transform.right = parent.transform.up;
             sprite.enabled = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.Return)) {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
             Fire();
         }
     }
 
-    public void Fire() {
-        if (Timer <= 0f) {
+    public void Fire()
+    {
+        if (Timer <= 0f)
+        {
+            // Insert noise in the harpoon's direction
+            transform.right = new Vector3(
+                        transform.right.x + UnityEngine.Random.Range(-noise, noise),
+                        transform.right.y + UnityEngine.Random.Range(-noise, noise),
+                        transform.right.z);
+
             GameObject obj = Instantiate(harpoonPrefab, transform.position, transform.rotation);
             sprite.enabled = false;
             var harpoon = obj.GetComponent<Harpoon>();
             harpoon.Parent = parent;
-            Timer = Cooldown; 
+            Timer = Cooldown;
         }
     }
 
-    public void LookAt(Vector3 point) {
+    public void LookAt(Vector3 point)
+    {
         transform.right = point - transform.position;
     }
 }
